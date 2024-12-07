9717b1e429a77ae47705ba2c6190f0cb6587caf1
S. Sarantsev <sarantsev.s@gmail.com>
2024-12-07 12:12:34 +0100

    depth helper default value

--- src/DepthHelper.js
+++ src/DepthHelper.js
@@ -1,12 +1,17 @@
-import {Camera} from 'another-webgpu';
+import {Camera, Vector2} from 'another-webgpu';
 
 class DepthHelper {
 	constructor(camera = new Camera(), element = document.createElement('')) {
+		const screen = new Vector2();
+
 		this.getDepth = (x = 0, y = 0) => {
 			const rect = element.getBoundingClientRect();
 			screen.x = Math.round(x - rect.left);
 			screen.y = Math.round(y - rect.top);
-			return camera.depthBuffer[screenY * element.width + screenX];
+			if (!camera.depthBuffer) {
+				return 1;
+			}
+			return camera.depthBuffer[screen.y * element.width + screen.x] || 1;
 		};
 	}
 }
